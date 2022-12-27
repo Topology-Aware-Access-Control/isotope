6c465fbf2e4204116ad4700dd2299fadfd768458
Muhammad Haseeb <mh6218@nyu.edu>
2022-12-27 16:09:39 -0500

    creating rules files along with topology files

--- example-topologies/create.py
+++ example-topologies/create.py
@@ -10,11 +10,13 @@ services:
 import json
 import yaml
 import sys
+import copy
 
 tree_levels = sys.argv[1] # 2
 tree_m = sys.argv[2] # 5
 calls_m = sys.argv[3] # 2
 filename = f"tree-{tree_levels}-{tree_m}-{calls_m}.yaml" # tree-2-5.yaml
+root_name = "svc-0"
 
 def write_topology(services):
     topology = {
@@ -44,16 +46,62 @@ def write_topology(services):
         f.write(yaml.dump(topology))
     print("total services: ", len(services_list))
 
+def dfs(graph, curr_node, rules):
+    if ("script" not in graph[curr_node]) or (len(graph[curr_node]["script"]) < 1):
+        return [",".join(rules)]
+    
+    res = []
+    if "script" in graph[curr_node]:
+        for ele in graph[curr_node]["script"]:
+            res += dfs(graph, ele, list(rules) + [ele])
+    
+    return res
+
+def break_paths_to_rules(paths):
+    rules = set()
+    for ele in paths:
+        p = ele.split(",")
+        p.reverse()
+        for i in range(0, len(p)):
+            r = list(p[i:])
+            r.reverse()
+            rules.add(",".join(r))
+    return list(rules)
+
+def get_service_names(services):
+    names = set()
+    for ele in services:
+        names.add(ele)
+    return list(names)
+
+
+def write_rules(services):
+    curr_rules = [root_name]
+    paths = dfs(copy.deepcopy(services), root_name, list(curr_rules))
+    rules = break_paths_to_rules(copy.deepcopy(paths))
+    names = get_service_names(services)
+    res = {
+        "rules": rules,
+        "services": names
+    }
+    fn = f"rules-example-{tree_levels}-{tree_m}-{calls_m}.json"
+    print("total paths: ", len(paths))
+    print("total rules: ", len(rules))
+    print("total nodes: ", len(names))
+
+    with open(fn, "w") as f:
+        f.write(json.dumps(res, indent=4))
+
 
 def create_tree(levels, k):
     services = {
-        'svc-0': {
+        f"{root_name}": {
             'isEntrypoint': True,
             'script': []
         }
     }
-    root = 'svc-0'
-    queue = [root]
+
+    queue = [root_name]
 
     while len(queue) > 0:
         top = queue[-1]
@@ -75,7 +123,9 @@ def create_tree(levels, k):
             services[ele] = {}
             queue = [ele] + queue
         
-    write_topology(services)
+    write_topology(copy.deepcopy(services))
+    write_rules(services)
+
 
 def create_chain(k):
     pass
